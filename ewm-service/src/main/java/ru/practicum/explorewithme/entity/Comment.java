33f1745dec7ba8c66a552aa36e6c097de8ef8ad7
69IstominaEA <69IstominaEA@region.cbr.ru>
2026-07-15 21:56:46 +0300

    Comments: добавлена таблица в schema.sql, добавлены Entity

--- ewm-service/src/main/java/ru/practicum/explorewithme/entity/Comment.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/entity/Comment.java
@@ -1,4 +1,54 @@
 package ru.practicum.explorewithme.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import ru.practicum.explorewithme.dto.comment.CommentStatus;
+import ru.practicum.explorewithme.dto.event.EventStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "comments", schema = "public")
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "author_name")
+    private String authorName;
+
+    @Column(name = "created")
+    private LocalDateTime created;
+
+    @Column(name = "updated")
+    private LocalDateTime updated;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private CommentStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "moderator_id", nullable = false)
+    private User moderator;
+
+    @Column(name = "moderated")
+    private LocalDateTime moderated;
+
+    @Column(name = "moderation_reason")
+    private String moderation_reason;
 }
+
